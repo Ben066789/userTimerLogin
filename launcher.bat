050195280dfd7dba73ef90b5bfea0be0d3501aa5
Ben066789 <dabenz8293@gmail.com>
2025-02-14 19:54:48 +0900

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -Command "Start-Process 'mshta.exe' -ArgumentList 'C:\Users\User\Desktop\login.hta' -Verb RunAs"
+powershell -Command "Start-Process 'mshta.exe' -ArgumentList '.\login.hta' -Verb RunAs"
